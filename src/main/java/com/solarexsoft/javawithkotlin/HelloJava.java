1ba975f2474326a8b7cb53651c090e2832052a65
Solarex <rh.hou.work@gmail.com>
2017-12-11 10:38:03 +0800

    init project

---
+++ src/main/java/com/solarexsoft/javawithkotlin/HelloJava.java
@@ -0,0 +1,11 @@
+package com.solarexsoft.javawithkotlin;
+
+/**
+ * Created by houruhou on 11/12/2017.
+ */
+public class HelloJava {
+    public static void main(String[] args){
+        User user = new User(1, "solarex");
+        System.out.println(user);
+    }
+}
