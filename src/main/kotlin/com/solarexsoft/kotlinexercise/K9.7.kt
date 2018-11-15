317417fc5263314d33912148cc458ca6155e7291
houruhou <houruhou@icarbonx.com>
2018-11-15 17:53:33 +0800

    kotlin singleton with parameter

--- src/main/kotlin/com/solarexsoft/kotlinexercise/K9.7.kt
+++ src/main/kotlin/com/solarexsoft/kotlinexercise/K9.7.kt
@@ -7,3 +7,15 @@ package com.solarexsoft.kotlinexercise
 object TestMutable {
     val list = listOf<Int>(1, 2, 3)
 }
+
+open class SingletonHolder<out T, in A>(creator: (A) -> T) {
+    private var creator: ((A) -> T)? = creator
+    @Volatile
+    private var instance: T? = null
+
+    fun getInstance(arg: A): T {
+        return instance ?: synchronized(this) {
+            instance ?: creator!!(arg).also { instance = it }
+        }
+    }
+}
