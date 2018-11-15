7781e1bed9c6a39e8544c671bcbd766e229b6d98
houruhou <houruhou@icarbonx.com>
2018-11-15 17:58:34 +0800

    kotlin object

--- src/main/kotlin/com/solarexsoft/kotlinexercise/K9.7.kt
+++ src/main/kotlin/com/solarexsoft/kotlinexercise/K9.7.kt
@@ -19,3 +19,26 @@ open class SingletonHolder<out T, in A>(creator: (A) -> T) {
         }
     }
 }
+
+class Context
+class DBManager private constructor(context: Context) {
+    init {
+        println("DBManager initialized")
+    }
+    companion object : SingletonHolder<DBManager, Context>(::DBManager)
+
+    fun insert() {
+        println("insert data into db")
+    }
+
+    fun delete() {
+        println("delete data from db")
+    }
+}
+
+fun main(args: Array<String>) {
+    val context = Context()
+    DBManager.getInstance(context).insert()
+    DBManager.getInstance(context).insert()
+    DBManager.getInstance(context).delete()
+}
