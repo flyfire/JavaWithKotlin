600e3f5bbc1bd344b6353c97bca7d3bf0eb0c2ca
Solarex <rh.hou.work@gmail.com>
2020-08-26 15:10:23 +0800

    next lucker

---
+++ src/main/kotlin/com/solarexsoft/practice/NextLucker.kt
@@ -0,0 +1,15 @@
+package com.solarexsoft.practice
+
+import java.util.*
+
+/**
+ * Created by Solarex on 2020/8/26/2:20 PM
+ * Desc:
+ */
+fun main(args: Array<String>) {
+    val allPeople = listOf<String>("教授", "侯茹厚", "余秋波", "朱奕豪", "兰怡芳", "黄文忠")
+    val sharedPeople = listOf<String>("侯茹厚", "兰怡芳")
+    val people = allPeople - sharedPeople
+    val index = Random().nextInt(5)
+    println("恭喜${people[index]}为我们提供下一次分享")
+}
