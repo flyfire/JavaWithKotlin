b173008ff24bba08b8c489f0ebf703678fcc7496
Solarex <rh.hou.work@gmail.com>
2020-08-26 15:17:29 +0800

    fix

--- src/main/kotlin/com/solarexsoft/practice/NextLucker.kt
+++ src/main/kotlin/com/solarexsoft/practice/NextLucker.kt
@@ -10,6 +10,6 @@ fun main(args: Array<String>) {
     val allPeople = listOf<String>("教授", "侯茹厚", "余秋波", "朱奕豪", "兰怡芳", "黄文忠")
     val sharedPeople = listOf<String>("侯茹厚", "兰怡芳")
     val people = allPeople - sharedPeople
-    val index = Random().nextInt(5)
+    val index = Random().nextInt(people.size)
     println("恭喜${people[index]}为我们提供下一次分享")
 }
