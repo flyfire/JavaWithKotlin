916c58fc5360c43d1eb577e76507f063e14d9a03
Solarex <rh.hou.work@gmail.com>
2021-03-09 10:30:05 +0800

    data class

--- src/main/kotlin/com/solarexsoft/kotlinexercise/DataTest.kt
+++ src/main/kotlin/com/solarexsoft/kotlinexercise/DataTest.kt
@@ -1,14 +1,24 @@
 package com.solarexsoft.kotlinexercise
 
+import com.google.gson.Gson
+
 abstract class AbsQuestion(
     val questionId: String = ""
 )
 
 data class WordQuestion(
     val correctOptions: String = ""
-) : AbsQuestion()
+) : AbsQuestion() {
+
+}
 
 fun main() {
     val word = WordQuestion("hello")
     println(word)
+    val json = """{
+        |"questionId": "1111",
+        |"correctOptions": "aaa"
+        |}""".trimMargin()
+    val question = Gson().fromJson(json, WordQuestion::class.java)
+    println(question)
 }
