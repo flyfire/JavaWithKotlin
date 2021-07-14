ef19f5cecf3c917c99f18df69692872be42ff84d
Solarex <rh.hou.work@gmail.com>
2021-07-14 14:44:22 +0800

    substring

---
+++ src/main/kotlin/com/solarexsoft/practice/TestSubString.kt
@@ -0,0 +1,13 @@
+package com.solarexsoft.practice
+
+fun main() {
+    val str = "When they played, I'd sing along. It made me smile."
+    println(str.substring(5, 9))
+    println(str.substring(10, 16))
+    println(str.substring(17, 20))
+    println(str.subSequence(21, 25))
+    println(str.substring(26, 31))
+    println(str.substring(33, 35))
+    println(str.subSequence(41, 43))
+    println(str.substring(44, 49))
+}
