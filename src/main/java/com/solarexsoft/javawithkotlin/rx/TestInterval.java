1b735b0e703ff3a8e38c4a1e1041f7be815c5ae7
Solarex <rh.hou.work@gmail.com>
2024-04-17 11:49:28 +0800

    interval

--- src/main/java/com/solarexsoft/javawithkotlin/rx/TestInterval.java
+++ src/main/java/com/solarexsoft/javawithkotlin/rx/TestInterval.java
@@ -1,6 +1,7 @@
 package com.solarexsoft.javawithkotlin.rx;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import org.jetbrains.annotations.NotNull;
@@ -17,7 +18,7 @@ public class TestInterval {
                     @Override
                     public Long apply(@NotNull Long aLong) throws Exception {
                         if (aLong != 0 && aLong % 5 == 0) {
-                            throw new RuntimeException("hit");
+                            throw new RuntimeException("map hit");
                         } else {
                             return aLong;
                         }
@@ -34,7 +35,27 @@ public class TestInterval {
                         System.out.println("exception = " + throwable.getLocalizedMessage());
                     }
                 });
-
+        Observable.interval(0, 1, TimeUnit.SECONDS)
+                .flatMap(new Function<Long, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(@NotNull Long aLong) throws Exception {
+                        if (aLong != 0 && aLong % 5 == 0) {
+                            throw new RuntimeException("flatmap hit");
+                        } else {
+                            return Observable.fromArray(String.valueOf(aLong), "solarex");
+                        }
+                    }
+                }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                System.out.println("world " + s);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                System.out.println("exception = " + throwable.getLocalizedMessage());
+            }
+        });
         try {
             Thread.sleep(10 * 1000);
         } catch (Throwable throwable) {
