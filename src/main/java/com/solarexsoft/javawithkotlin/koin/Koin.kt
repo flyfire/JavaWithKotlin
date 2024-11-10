0ee8d9bc725a27d4272e538f8254520cdb1b2b00
Solarex <rh.hou.work@gmail.com>
2024-11-10 14:06:33 +0800

    koin

--- src/main/java/com/solarexsoft/javawithkotlin/koin/Koin.kt
+++ src/main/java/com/solarexsoft/javawithkotlin/koin/Koin.kt
@@ -15,17 +15,21 @@ class KoinContext {
     private val definitions = mutableMapOf<Class<*>, BeanDefinition<*>>()
     private val instances = mutableMapOf<Class<*>, Any>()
     private val creatingStack = mutableListOf<Class<*>>()
+    private var currentParameters: Map<String, Any> = emptyMap()
 
     fun <T : Any> register(clazz: Class<T>, definition: BeanDefinition<T>) {
-        definitions[clazz] = definition
+        definitions[clazz] = definition as BeanDefinition<*>
     }
 
     fun <T : Any> get(clazz: Class<T>, parameters: Map<String, Any> = emptyMap()): T {
         if (creatingStack.contains(clazz)) {
             throw IllegalStateException("circular dependency detected: ${creatingStack.joinToString { " -> " }} -> ${clazz.name}")
         }
+        val oldParameters = currentParameters
         try {
             creatingStack.add(clazz)
+
+            val paramsToUse = if (parameters.isEmpty()) currentParameters else parameters
 
             if (definitions[clazz]?.isSingleton == true && instances.containsKey(clazz)) {
                 return instances[clazz] as T
@@ -33,7 +37,7 @@ class KoinContext {
 
             val definition = definitions[clazz] ?: throw IllegalStateException("No definition found for ${clazz.name}")
 
-            val instance = (definition as BeanDefinition<T>).factory.invoke(this, parameters)
+            val instance = (definition.factory as KoinContext.(Map<String, Any>) -> T).invoke(this, paramsToUse)
 
             if (definition.isSingleton) {
                 instances[clazz] = instance
@@ -42,7 +46,14 @@ class KoinContext {
             return instance
         } finally {
             creatingStack.removeLast()
+            currentParameters = oldParameters
         }
+    }
+
+    fun clear() {
+        definitions.clear()
+        instances.clear()
+        creatingStack.clear()
     }
 }
 
@@ -53,7 +64,79 @@ class Module {
         definitions.add(T::class.java to BeanDefinition(definition, true))
     }
 
-    inline fun <reified T : Any> factory(noinline defintion: KoinContext.(Map<String, Any>) -> T) {
-        definitions.add(T::class.java to BeanDefinition(defintion, false))
+    inline fun <reified T : Any> factory(noinline definition: KoinContext.(Map<String, Any>) -> T) {
+        definitions.add(T::class.java to BeanDefinition(definition, false))
     }
+}
+
+class Koin {
+    val context = KoinContext()
+
+    fun startKoin(vararg modules: com.solarexsoft.javawithkotlin.koin.Module) {
+        modules.forEach { module ->
+            module.definitions.forEach {  (clazz, definition) ->
+                context.register(clazz as Class<Any>, definition as BeanDefinition<Any>)
+            }
+        }
+    }
+
+    inline fun <reified T : Any> get(vararg parameters: Pair<String, Any>) : T {
+        return context.get(T::class.java, parameters.toMap())
+    }
+}
+
+object GlobalKoin {
+    val koin = Koin()
+
+    fun startKoin(vararg modules: com.solarexsoft.javawithkotlin.koin.Module) = koin.startKoin(*modules)
+
+    inline fun <reified T: Any> get(vararg parameters: Pair<String, Any>): T = koin.get(*parameters)
+}
+
+inline fun <reified T : Any> KoinContext.get(vararg parameters: Pair<String, Any>): T {
+    return get(T::class.java, parameters.toMap())
+}
+
+data class Config(val url: String)
+class Repository(private val config: Config)
+class Service(private val repository: Repository)
+class Controller(
+    private val service: Service,
+    private val debugMode: Boolean
+)
+
+fun main() {
+    val myModule = Module().apply {
+        single { params ->
+            println("creating Config with parameters: $params")
+            Config(params["baseUrl"] as String)
+        }
+
+        single { params ->
+            println("creating Repository with parameters: $params")
+            Repository(get())
+        }
+
+        single { params ->
+            println("creating Service with parameters: $params")
+            Service(get())
+        }
+
+        single { params ->
+            println("creating Controller with parameters: $params")
+            Controller(
+                service = get(),
+                debugMode = params["debug"] as? Boolean ?: false
+            )
+        }
+    }
+
+    GlobalKoin.startKoin(myModule)
+
+    val controller = GlobalKoin.get<Controller>(
+        "baseUrl" to "https://www.goolge.com",
+        "debugMode" to true
+    )
+
+    println("Controller created: $controller")
 }
